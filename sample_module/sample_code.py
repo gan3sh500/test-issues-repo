7748a5c447b8fbb029f21320351d9773898371ff
Mazhar Shaikh <mazharshaikh94@gmail.com>
2021-02-20 19:18:20 +0900

    Added Sample code

---
+++ sample_module/sample_code.py
@@ -0,0 +1,10 @@
+import ast
+from datetime import time, date 
+
+class SampleClass(object):
+    def __init__(self):
+        pass
+    def sample_public_function(self):
+        pass
+    def __sample_private_function__(self):
+        pass
