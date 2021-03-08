69e519f4baaa286dcd144b429367662c6067b056
Mazhar WPC <mazharshaikh94@gmail.com>
2021-03-08 20:02:11 +0900

    Added more statements for testing code parsing

--- sample_module/sample_code.py
+++ sample_module/sample_code.py
@@ -1,10 +1,14 @@
 import ast
+import numpy as np
 from datetime import time, date 
 
 class SampleClass(object):
-    def __init__(self):
-        pass
-    def sample_public_function(self):
-        pass
-    def __sample_private_function__(self):
-        pass
+    def __init__(self, argument1, keyword_argument = 'sample'):
+        self.attribute1 = argument1
+        self.attribute2 = self.__sample__private_function__(keyword_argument)
+        self.sample_public_function(self.attribute1)
+    def sample_public_function(self, argument):
+        return self.attribute1, argument 
+
+    def __sample_private_function__(self, argument):
+        return argument
